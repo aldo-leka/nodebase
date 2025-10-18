18f42446f1cff852eb5def050d8e7591afb117d1
Aldo Leka <aldo.leka@live.com>
2025-10-18 21:08:30 +0200

    04: authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,19 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
+import { LogoutButton } from "./logout";
 
 const Page = async () => {
-  const queryClient = getQueryClient();
+  await requireAuth();
 
-  /*
-  It is as fast as with a caller in a server component.
-  Server component has no data at this moment.
-  We are leveraging trpc prefetch and we are 
-  populating tanstack query client state with it.
-  ...Client component will be populated by server 
-  component.
-   */
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  const data = await caller.getUsers();
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>Loading...</p>}>
-         <Client />
-        </Suspense>
-      </HydrationBoundary>
+      protected server component
+      <div>
+        {JSON.stringify(data, null, 2)}
+      </div>
+      <LogoutButton />
     </div>
   )
 };
