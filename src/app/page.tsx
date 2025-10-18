a1a82831caa8fef7446e04fa520522b6a4d84361
Aldo Leka <aldo.leka@live.com>
2025-10-18 11:22:07 +0200

    03: trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import prisma from "@/lib/db";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 const Page = async () => {
-  const users = await prisma.user.findMany();
+  const queryClient = getQueryClient();
+
+  /*
+  It is as fast as with a caller in a server component.
+  Server component has no data at this moment.
+  We are leveraging trpc prefetch and we are 
+  populating tanstack query client state with it.
+  ...Client component will be populated by server 
+  component.
+   */
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(users)}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>Loading...</p>}>
+         <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   )
 };
