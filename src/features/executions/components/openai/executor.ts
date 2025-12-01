452e2247796dee453bd724673987e384bc191f80
Aldo Leka <aldo.leka@live.com>
2025-12-01 11:45:42 +0100

    26: discord slack nodes

--- src/features/executions/components/openai/executor.ts
+++ src/features/executions/components/openai/executor.ts
@@ -24,6 +24,7 @@ type OpenAiData = {
 export const openAiExecutor: NodeExecutor<OpenAiData> = async ({
     data,
     nodeId,
+    userId,
     context,
     step,
     publish,
@@ -75,6 +76,7 @@ export const openAiExecutor: NodeExecutor<OpenAiData> = async ({
         return prisma.credential.findUnique({
             where: {
                 id: data.credentialId,
+                userId,
             },
         });
     });
